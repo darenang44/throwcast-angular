41eaca4c24527e1b5b47356555dbe675b7c39759
Daren Ang <darenang44@gmail.com>
2016-08-09 12:45:06 -0700

    comment out linter

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -3,4 +3,7 @@ angular.module('throwcast.auth', [])
 .controller('AuthController', function($scope, $http){
   $scope.signup = 'Signup';
   $scope.signin = 'Signin';
+  $scope.submit = function () {
+    console.log('submitted');
+  }
 });

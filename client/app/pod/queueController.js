a990335bd46c214a4f2cebdcf502d2f9abce9387
Daren Ang <darenang44@gmail.com>
2016-08-11 16:12:29 -0700

    Add playlist routes

--- client/app/pod/queueController.js
+++ client/app/pod/queueController.js
@@ -4,20 +4,20 @@ angular.module('throwcast.queue')
   $scope.h1 = 'Podcast Queue';
 
   $scope.getQueue = function (userId) {
-    $http.get('user/' + userId + '/queue/').then(function (res) {
+    $http.get('api/user/' + userId + '/queue/').then(function (res) {
       $scope.queue = res.data;
     });
   };
   $scope.getQueue();
 
   $scope.addToQueue = function (userId, podcastId) {
-    $http.post('user/' + userId + '/queue/' + podcastId).then(function (res) {
+    $http.post('api/user/' + userId + '/queue/' + podcastId).then(function (res) {
       $scope.getQueue();
     });
   };
 
   $scope.deleteFromQueue = function (userId, podcastId) {
-    $http.delete('user/' + userId + '/queue/' + podcastId).then(function (res) {
+    $http.delete('api/user/' + userId + '/queue/' + podcastId).then(function (res) {
       $scope.getQueue();
     });
   };

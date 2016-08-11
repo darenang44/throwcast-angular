5e75cd63fd099e96bee1123093c7ba52f2613ae2
Daren Ang <darenang44@gmail.com>
2016-08-11 12:29:30 -0700

    Add queue logic

--- client/app/pod/queueController.js
+++ client/app/pod/queueController.js
@@ -2,4 +2,23 @@ angular.module('throwcast.queue')
 
 .controller('QueueController', function ($scope, $http) {
   $scope.h1 = 'Podcast Queue';
+
+  $scope.getQueue = function (userId) {
+    $http.get('user/' + userId + '/queue/').then(function (res) {
+      $scope.queue = res.data;
+    });
+  };
+  $scope.getQueue();
+
+  $scope.addToQueue = function (userId, podcastId) {
+    $http.post('user/' + userId + '/queue/' + podcastId).then(function (res) {
+      $scope.getQueue();
+    });
+  };
+
+  $scope.deleteFromQueue = function (userId, podcastId) {
+    $http.delete('user/' + userId + '/queue/' + podcastId).then(function (res) {
+      $scope.getQueue();
+    });
+  };
 });

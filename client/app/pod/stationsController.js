e08f38595923e57b4ef59ffb0e3c87e87cdaecaf
Daren Ang <darenang44@gmail.com>
2016-08-12 11:52:04 -0700

    Allow popular and stations to add to queue and playlist

--- client/app/pod/stationsController.js
+++ client/app/pod/stationsController.js
@@ -2,6 +2,7 @@ angular.module('throwcast.stations')
 
 .controller('StationsController', function ($scope, $http) {
   $scope.h1 = 'Stations';
+  $scope.message = 'Dummy message';
   $scope.news = [
     {name: 'CNN1', description: 'World News1', link: '1cnn.mp3'},
     {name: 'CNN2', description: 'World News2', link: '2cnn.mp3'},
@@ -34,4 +35,33 @@ angular.module('throwcast.stations')
       $scope.stationPodcasts = stationPodcasts;
     });
   };
+
+  $scope.addToQueue = function (userId, podcastId) {
+    $http.post('api/user/' + userId + '/queue/', {podcastId: podcastId}).then(function (res) {
+      $scope.message = $scope.stationPodcasts.name + ' has been added to your queue.';
+      $scope.getStations();
+    });
+  };
+
+  $scope.addPodToPlaylist = function (playlistId, podcastId) {
+    $http.post('api/playlist/' + playlistId + '/podcast/', {podcastId: podcastId}).then(function (res) {
+      $scope.message = $scope.stationPodcasts.name + ' has been added to ' + res.data.name + '.';
+      $scope.getStations();
+    });
+  };
+
+  $scope.unsubscribe = function (userId, stationId) {
+    $http.delete('api/user/' + userId + '/subscriptions/', {stationId: stationId}).then(function (res) {
+      $scope.message = 'Unsubscribed to ' + res.data.name + '.';
+      $scope.getStations();
+    });
+  };
+
+  $scope.subscribe = function (userId, stationId) {
+    $http.post('api/user' + userId + '/subscriptions/', {stationId: stationId}).then(function (res) {
+      $scope.message = 'Subscribed to ' + res.data.name + '.';
+      $scope.getStations();
+    });
+  };
+
 });
